1cfd74cfe91744075ffaae152aaa44784ac45883
unknown <dwrighttt504@gmail.com>
2021-06-17 10:42:44 -0500

    Finalized Version

--- levelset/src/App.js
+++ levelset/src/App.js
@@ -6,6 +6,7 @@ class Home extends Component {
     super(props);
     this.state = {
         cats: [''],
+        selectedOwnerName: '',
         searchTerm: '',
         selecetdCat: [
           {
@@ -25,14 +26,32 @@ class Home extends Component {
       selecetdCat: JSON.parse(localStorage.getItem('cats'))[0]
     })
   }
+  componentDidUpdate(ex1, ex2){
+    
+  }
+  overlayOn() {
+    document.getElementById("overlay").style.display = "block";
+    document.querySelector('#edit-birthdate-input').value = this.state.selecetdCat.Birthdate;
+  }
+  
+  overlayOff() {
+    document.getElementById("overlay").style.display = "none";
+  }
   chooseCat(e){
+    console.log(e);
     const selectedElm = JSON.parse(localStorage.getItem('cats'));
     // Add 1 to user viewcount on click
     selectedElm[e].viewCount = selectedElm[e].viewCount + 1;
+    
     // Set new cat array to local storage with updated viewcount
     localStorage.setItem('cats', JSON.stringify(selectedElm))
     this.setState({
       selecetdCat: JSON.parse(localStorage.getItem('cats'))[e]
+    })
+  }
+  selectOwnerName(e){
+    this.setState({
+      selectedOwnerName: e.target.value
     })
   }
   search(e){
@@ -47,6 +66,29 @@ class Home extends Component {
       cats: filteredArr
     })
   }
+  updateCat(e){
+    let updatedUrl = document.querySelector('#edit-thumbnail-url-input').value;
+    let updatedName = document.querySelector('#edit-name-input').value;
+    let updatedBirthday = document.querySelector('#edit-birthdate-input').value;
+    const selectedElm = JSON.parse(localStorage.getItem('cats'));
+    selectedElm[this.state.selecetdCat.ID] = {
+          "Name":updatedName === '' ? this.state.selecetdCat.Name : updatedName,
+          "ID":this.state.selecetdCat.ID,
+          "thumbnailUrl":updatedUrl === '' ? this.state.selecetdCat.thumbnailUrl:updatedUrl ,
+          "Birthdate":updatedBirthday === '' ? this.state.selecetdCat.Birthdate : updatedBirthday,
+          "ownerName":this.state.selectedOwnerName === '' ? this.state.selecetdCat.ownerName : this.state.selectedOwnerName,
+          "viewCount":this.state.selecetdCat.viewCount
+    }
+    // console.log(selectedElm);
+    localStorage.setItem('cats', JSON.stringify(selectedElm))
+    window.location.reload();
+  }
+  deleteCat(e){
+    const selectedElm = JSON.parse(localStorage.getItem('cats'));
+    selectedElm.splice(this.state.selecetdCat.ID, 1)
+    localStorage.setItem('cats', JSON.stringify(selectedElm))
+    window.location.reload();
+  }
   render() { 
     // If there are no cats array on localStorage make one
     if (!localStorage.cats){
@@ -55,56 +97,64 @@ class Home extends Component {
           "Name":"Adam",
           "ID":0,
           "thumbnailUrl":"https://images.pexels.com/photos/774731/pexels-photo-774731.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-          "Birthdate":"01-01-2011",
-          "Owner Name":"Esther",
+          "Birthdate":"2011-01-01",
+          "ownerName":"Esther",
           "viewCount":0
         },
         {
           "Name":"Bernard",
           "ID":1,
           "thumbnailUrl":"https://images.pexels.com/photos/1170986/pexels-photo-1170986.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-          "Birthdate":"02-02-2012",
-          "Owner Name":"Darkayla",
+          "Birthdate":"2012-02-02",
+          "ownerName":"Darkayla",
           "viewCount":0
         },
         {
           "Name":"Calvin",
           "ID":2,
           "thumbnailUrl":"https://images.pexels.com/photos/1543793/pexels-photo-1543793.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-          "Birthdate":"03-03-2013",
-          "Owner Name":"Cordell",
+          "Birthdate":"2013-03-03",
+          "ownerName":"Cordell",
           "viewCount":0
         },
         {
           "Name":"Daniel",
           "ID":3,
           "thumbnailUrl":"https://images.pexels.com/photos/2558605/pexels-photo-2558605.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-          "Birthdate":"04-04-2014",
-          "Owner Name":"Bishop",
+          "Birthdate":"2014-04-04",
+          "ownerName":"Bishop",
           "viewCount":0
         },
         {
           "Name":"Ezekiel",
           "ID":4,
           "thumbnailUrl":"https://images.pexels.com/photos/236606/pexels-photo-236606.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-          "Birthdate":"05-05-2015",
-          "Owner Name":"Alexander",
+          "Birthdate":"2015-05-05",
+          "ownerName":"Alexander",
           "viewCount":0
         }
       ]
       localStorage.setItem('cats', JSON.stringify(catObject))
     }
+    const selectedElm = JSON.parse(localStorage.getItem('cats'));
+    selectedElm.forEach((elm, index) => {
+      elm.ID = index
+    })
+    localStorage.setItem('cats', JSON.stringify(selectedElm))
 
     // Render cats object
     const cats = this.state.cats.map((elm, index) => {
-      return <div onClick={(e) => this.chooseCat(elm.ID)} key={elm.ID} className='left-cat-box'>
+      return <div onClick={(e) => this.chooseCat(index)} key={elm.ID} className='left-cat-box'>
         <img alt={elm.Name} className='col-sm-4, left-cat-box-img' src={elm.thumbnailUrl} />
         <span>{elm.Name}</span>
         <p>{elm.Birthdate}</p>
       </div>
       }
     )
-
+    const ownerNames = this.state.cats.map((elm, index) => {
+      return <option key={index} value={elm.ownerName}>{elm.ownerName}</option>
+    })
+    console.log(this.state);
     return ( 
       <div className='container'>
         <h1>Cats</h1>
@@ -118,10 +168,36 @@ class Home extends Component {
           <div id='div-right' className="col-sm-12, col-lg-8">
             <div id='cat-details-main'>
               <img id='cat-details-img' alt='alt' src={this.state.selecetdCat.thumbnailUrl} />
-              <p>{this.state.selecetdCat.Name}</p>
-              <p>{this.state.selecetdCat.Birthdate}</p>
-              <p>{this.state.selecetdCat.ownerName}</p>
+              <p>Name: {this.state.selecetdCat.Name}</p>
+              <p>D-O-B: {this.state.selecetdCat.Birthdate}</p>
+              <p>Owner Name: {this.state.selecetdCat.ownerName}</p>
               <p>Number of views: {this.state.selecetdCat.viewCount} times</p>
+              <div id='edit-delete-cont'>
+                <span onClick={(e) => this.overlayOn()}>Edit</span> | <span onClick={(e) => this.deleteCat(e)}>Delete</span>
+              </div>
+            </div>
+          </div>
+          <div id='overlay'>
+            <h2>Edit Cat</h2>
+            <div id='edit-box-main'>
+              <div className='edit-box1' >
+                <label htmlFor="thumbnail-url">Thumbnail Url :</label> <br></br>
+                <label htmlFor="fname">Name :</label> <br></br>
+                <label htmlFor="birthdate">Birthdate :</label> <br></br>
+                <label htmlFor="owner">Owner :</label> <br></br>
+              </div>
+
+              <div className='edit-box2'>
+                <input className='form-control' type="text" id="edit-thumbnail-url-input" name="thumbnail-url" />
+                <input className='form-control' type="text" id="edit-name-input" name="name" />
+                <input className='form-control' type="date" id="edit-birthdate-input" name="birthdate" min="2000-01-01" max="2020-12-31"  />
+                <select id="edit-owner-input" name="owner" onChange={(e) => this.selectOwnerName(e)}>
+                  {ownerNames}
+                </select>
+              </div>
+              <div id='save-delete-cont'>
+                <span onClick={(e) => this.updateCat(e)}>Save</span> <span>|</span> <span onClick={(e) => this.deleteCat(e)}>Delete</span>
+              </div>
             </div>
           </div>
         </div>
